cb6413dc16dc68f08d6940ed7043c959a1b019f8
furiouswhqle <148171479+furiouswhqle@users.noreply.github.com>
2025-04-24 17:14:17 -0500

    Update NoCropTrampleMixin.java

--- src/main/java/furiouswhqle/mixin/NoCropTrampleMixin.java
+++ src/main/java/furiouswhqle/mixin/NoCropTrampleMixin.java
@@ -1,27 +1,24 @@
 package furiouswhqle.mixin;
 
-import net.minecraft.block.Block;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FarmlandBlock;
 import net.minecraft.entity.Entity;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(FarmlandBlock.class)
-public abstract class NoCropTrampleMixin extends Block {
-	public NoCropTrampleMixin(Settings settings) {
-		super(settings);
-	}
-	@Inject(method = "onLandedUpon", at = @At("HEAD"), cancellable = true)
-	public void NoCropTrample(World world, BlockState state, BlockPos pos, Entity entity, double fallDistance, CallbackInfo ci) {
-		entity.handleFallDamage(fallDistance, 1f, world.getDamageSources().fall());
-		ci.cancel();
+public class NoCropTrampleMixin {
+	@Redirect(
+		method = "onLandedUpon",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/block/FarmlandBlock;setToDirt(Lnet/minecraft/entity/Entity;Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"
+		)
+	)
+	private void cancelFarmlandTrample(Entity entity, BlockState state, World world, BlockPos pos) {
 	}
 }
