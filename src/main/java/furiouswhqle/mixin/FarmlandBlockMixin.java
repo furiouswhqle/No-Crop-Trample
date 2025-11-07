6203c225b100434872bedcf26ddaaa393f3a9080
furiouswhqle <148171479+furiouswhqle@users.noreply.github.com>
2025-11-06 19:27:16 -0600

    Refactor FarmlandBlockMixin to FarmBlockMixin

--- src/main/java/furiouswhqle/mixin/FarmlandBlockMixin.java
+++ src/main/java/furiouswhqle/mixin/FarmlandBlockMixin.java
@@ -3,22 +3,20 @@ package furiouswhqle.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.block.FarmBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.Level;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.FarmlandBlock;
-import net.minecraft.entity.Entity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
-@Mixin(FarmlandBlock.class)
-public class FarmlandBlockMixin {
-	@Redirect(
-		method = "onLandedUpon",
-		at = @At(
+@Mixin(FarmBlock.class)
+public class FarmBlockMixin {
+	@Redirect (
+		method = "fallOn",
+		at = @At (
 			value = "INVOKE",
-			target = "Lnet/minecraft/block/FarmlandBlock;setToDirt(Lnet/minecraft/entity/Entity;Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"
+			target = "Lnet/minecraft/world/level/block/FarmBlock;turnToDirt(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V"
 		)
 	)
-	private void Cancel(Entity entity, BlockState state, World world, BlockPos pos) {
-	}
+	private void Cancel(Entity entity, BlockState state, Level level, BlockPos pos) {}
 }
