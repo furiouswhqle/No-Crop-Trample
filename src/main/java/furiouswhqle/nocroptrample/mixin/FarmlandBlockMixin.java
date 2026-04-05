f80c159d9f11da6b17dfd86be7c71619be83ee2b
furiouswhqle <148171479+furiouswhqle@users.noreply.github.com>
2026-04-05 12:38:29 -0500

    Add files via upload

---
+++ src/main/java/furiouswhqle/nocroptrample/mixin/FarmlandBlockMixin.java
@@ -0,0 +1,22 @@
+package furiouswhqle.nocroptrample.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.block.FarmlandBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.Level;
+
+@Mixin(FarmlandBlock.class)
+public class FarmlandBlockMixin {
+	@Redirect (
+		method = "fallOn",
+		at = @At (
+			value = "INVOKE",
+			target = "Lnet/minecraft/world/level/block/FarmlandBlock;turnToDirt(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V"
+		)
+	)
+	private void Cancel(Entity entity, BlockState state, Level level, BlockPos pos) {}
+}
